2cc7ebe4744e8a28eb84f6d736fb94ee14d15e98
Mohsin <mdmohsinhaider6@gmail.com>
2023-12-31 13:21:04 +0530

    vairiable

--- vaiables.js
+++ vaiables.js
@@ -3,5 +3,12 @@ let accountEmail = "devil@hell.com"
 var accountPassword = "121vjk848*&^"
 accountCity = "Hell"  
 
-accountId = 2;
-console.log(accountId); 
+// accountId = 2; not allowed
+// console.log(accountId); 
+
+accountEmail = "newvalue@v.com"
+accountPassword= "ertfghu567y"
+accountCity ="Bangalur" // it possiable but it is not
+let accountState; // undefined
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
