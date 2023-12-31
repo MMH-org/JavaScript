8d679b9894390fa4596fee03a579fdf8c7daf536
Mohsin <mdmohsinhaider6@gmail.com>
2023-12-31 13:56:03 +0530

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //  treat all Js Code as newer version
+
+// alert()  // we are useing node js ,not browser
+
+console.log("Data Types");
+
+let name ="Md Mohsin Haider";
+let age = 22;
+let isLoggedIn= false;
+
+// number  => to power 53;
+// BigInt
+// String =" "
+// Boolean = true/false
+// null=>  standalone value / specel type its blank
+// undefined =>  
+// Symbol => unique
+
+// boject
+// typeof
+// console.table([age,name,isLoggedIn]);
+// console.log(typeof null); // object 
+// console.log(typeof undefined); // undefined
