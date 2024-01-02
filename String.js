c85205b096c8e800dd562546f5e40ed5f19b9f8f
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-02 18:55:38 +0530

    Strings

---
+++ String.js
@@ -0,0 +1,6 @@
+const name = "Md mohsin Haider ";
+const repoCount = 500;
+
+// console.log( name + repoCount + " Value"); // not rec
+
+console.log(`Hello My name is ${name} my repo count is ${repoCount}`)
