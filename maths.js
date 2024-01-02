95ab3632a1a9f39b71d5d9947d131cbccb3407a3
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-02 23:40:21 +0530

    Maths random()

---
+++ maths.js
@@ -0,0 +1,13 @@
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+
+// console.log(Math.min(4,5,3,2,8,1));
+// console.log(Math.max(4,5,3,2,8,1));
+
+console.log(Math.random());
