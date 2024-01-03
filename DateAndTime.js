c7fea2dd983219f30a08bfa86c9f0d14500b725b
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-03 19:24:29 +0530

    Date and time final

--- DateAndTime.js
+++ DateAndTime.js
@@ -27,5 +27,18 @@ let myTimeStamp =Date.now();
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
 
-console.log(Date.now()/1000);
+// console.log(Date.now()/1000) ;
+// console.log(Math.floor(Date.now()/1000));
 
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+
+newDate.toLocaleString('default',{
+    weekday:"long"
+    
+});
