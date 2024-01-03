63db688b175da6e9214ebe875316e970ded02be4
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-03 21:39:11 +0530

    Array slice,splice

--- Array.js
+++ Array.js
@@ -1,1 +1,28 @@
 const myArray = [0,1,2,3,4,5,6,7,8,9,"mohsin"];
+const myHero = ["saktiman", "naagraj"];
+
+const myArray2= new Array(1,2,3,4,5);
+// console.log(myArray[1]);
+
+// Array methods
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop();
+
+
+// myArray.unshift(9); // add in first
+// myArray.shift()
+
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf("9"));
+
+
+// const newArray=myArray.join()
+
+// console.log(myArray);
+// console.log(typeof newArray);
+
+
+
+// 
