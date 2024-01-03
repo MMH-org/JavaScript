0b913a5aa17ac9eb6156cd4b74ef35c2f4f521c3
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-04 01:32:39 +0530

    Array part 2  isArray and from

--- ArrayPart2.js
+++ ArrayPart2.js
@@ -15,8 +15,16 @@ const all_new_Heros = [...marvel_heros,...dc_heros];
 // console.log(all_new_Heros);
 
 
-const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]];
 
-const real_another_array = another_array.flat(Infinity);
-console.log(real_another_array);
-console.log(typeof(real_another_array));
+// const real_another_array = another_array.flat(Infinity);
+// console.log(real_another_array);
+// console.log(typeof(real_another_array));
+
+
+console.log(Array.isArray("mohsin")); // false
+console.log(Array.from("mohsin")); // [ 'm', 'o', 'h', 's', 'i', 'n' ] 
+
+
+
+
