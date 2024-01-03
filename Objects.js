e3453ba9288bc098f04b86fb86f44bef07683c07
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-04 01:47:26 +0530

    objects literals

---
+++ Objects.js
@@ -0,0 +1,14 @@
+// singleton
+// Object.create
+
+
+
+// objects literals
+const JsUser = {
+    name:"mohsin",
+    age:18,
+    location:"hell",
+    email:"devil@google.com",
+    isLoggedIn:false,
+    lastLoggedIn:["monday", "Saturday"]
+}
