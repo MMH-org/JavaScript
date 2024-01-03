9ece1ec3c6bef12520c310db45e92a2d3a8744e1
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-04 03:11:21 +0530

    Objects

---
+++ Objects_2.js
@@ -0,0 +1,32 @@
+// const tinderUser = new Object(); // singleton
+const tinderUser={}; // non singleton
+
+tinderUser.id = "123abc"
+tinderUser.name="Devil"
+tinderUser.isLoggedIn = false;
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    emain: "some@gmail.com",
+    fullname:{
+        userFullName:{
+            firstname:"mohsin",
+            lastName:"Haider"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName.firstname);
+
+
+const obj1 = {1:"a", 2:"b"};
+const obj2 = {3:"a", 4:"b"};
+
+const obj3 = {obj1, obj2};
+console.log(obj3);
+
+
+
+
