f67b5b4e733be85241e7ef568e0809a967dc0262
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-05 12:35:04 +0530

    first part

---
+++ Global_and_local_scope.js
@@ -0,0 +1,18 @@
+// let a = 10;
+// const b = 20;
+// var c = 30;
+
+
+// var c = 300;
+let c = 300; // global Scope;
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    // inner scope
+    // var c = 30;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
