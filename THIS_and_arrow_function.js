dcb073fc9754ba1754a8e419a384a2396d9ec836
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-06 01:24:19 +0530

    Object creating

---
+++ THIS_and_arrow_function.js
@@ -0,0 +1,8 @@
+const user = {
+    username:"mohsin",
+    price:999,
+
+    welcomeMassage:function(){
+        console.log(`${this.username} Welocome to website`); // this is current cont..
+    }
+}
