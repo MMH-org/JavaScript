0212101a79e37cc1b9189ab08df2699be56a4844
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-06 03:41:34 +0530

    this key wo

--- THIS_and_arrow_function.js
+++ THIS_and_arrow_function.js
@@ -6,3 +6,13 @@ const user = {
         console.log(`${this.username} Welocome to website`); // this is current cont..
     }
 }
+// user.welcomeMassage // refrance
+// user.welcomeMassage();
+
+// user.username = "sam";
+// user.welcomeMassage();
+
+
+
+// console.log(this);
+
