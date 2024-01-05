b6bc7729289df7471b67aee91dfcdea6b6ef0865
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-06 01:19:18 +0530

    Scope level and mini hoisting in javascript

--- vaiables.js
+++ vaiables.js
@@ -6,9 +6,9 @@ accountCity = "Hell"
 // accountId = 2; not allowed
 // console.log(accountId); 
 
-accountEmail = "newvalue@v.com"
-accountPassword= "ertfghu567y"
-accountCity ="Bangalur" // it possiable but it is not
+// accountEmail = "newvalue@v.com"
+// accountPassword= "ertfghu567y"
+// accountCity ="Bangalur" // it possiable but it is not
 let accountState; // undefined
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
