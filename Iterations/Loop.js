e2241f7f7bcef9a5495306533fe8e9753ec3cbd1
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-08 02:50:28 +0530

    for Loop first method

--- Iterations/Loop.js
+++ Iterations/Loop.js
@@ -1,5 +1,16 @@
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element);
+try {
+    for (let index = 0; index < 10; index++) {
+        const element = index;
+        // console.log(element);
+        if (element===5) {
+            // console.log("5 number hai bhai eha pai");
+        }
+        
+    }
+    // console.log("hello  bhai eha khatam karo hogya ");
     
+
+
+} catch (error) {
+    console.log("catch");
 }
