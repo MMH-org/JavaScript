359ab6eda4abe13bb21e0d70b56fbe1baccfa324
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-07 19:13:33 +0530

    IIFE function

---
+++ IIFE.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions IIFE
+
+
+// function chai() {
+//     console.log(`DB CONNECTED`);
+// }
+// chai();
+
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+})()
+
