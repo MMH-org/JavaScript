cb21e871737350134ca203215a3379e40a31727c
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-08 23:26:23 +0530

    High Order Array loops

---
+++ Iterations/High_Order_Array_loops.js
@@ -0,0 +1,47 @@
+// for of
+
+
+// ["" , "" , ""]
+// [{} , {} , {}] //  array 
+[]
+const arrs = [
+    {
+        color: "red",
+        value: "#f00"
+    },
+    {
+        color: "green",
+        value: "#0f0"
+    },
+    {
+        color: "blue",
+        value: "#00f"
+    },
+    {
+        color: "cyan",
+        value: "#0ff"
+    },
+    {
+        color: "magenta",
+        value: "#f0f"
+    },
+    {
+        color: "yellow",
+        value: "#ff0"
+    },
+    {
+        color: "black",
+        value: "#000"
+    }
+]
+
+const array = [1,2,3,4,5,6,7,8,9,0]
+
+try {
+    
+    for (const iterator of array) {
+        console.log(iterator);
+    }
+} catch (error) {
+    console.log(`🏳️ ha Bhai hogy error ${error}`);
+}
