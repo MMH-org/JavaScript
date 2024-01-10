839720722f68caaec446d7276997a9201111c403
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-10 22:16:32 +0530

    for each

---
+++ Iterations/foreach.js
@@ -0,0 +1,14 @@
+const coding = ["js","ruby","java","pythone","cpp"];
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+coding.forEach( (valueofInsideArray)=>{
+    console.log(valueofInsideArray);
+} )
+
+
+
+
+
