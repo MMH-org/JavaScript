d08f4c2c050971f5ea580ddbb916522b895ec367
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-10 22:27:06 +0530

    for each

--- Iterations/foreach.js
+++ Iterations/foreach.js
@@ -1,12 +1,28 @@
 const coding = ["js","ruby","java","pythone","cpp"];
 
+
+
 // coding.forEach( function (item) {
 //     console.log(item);
 // } )
 
-coding.forEach( (valueofInsideArray)=>{
-    console.log(valueofInsideArray);
-} )
+
+
+
+
+// coding.forEach( (valueofInsideArray)=>{
+//     console.log(valueofInsideArray);
+// } )
+
+
+
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
 
 
 
