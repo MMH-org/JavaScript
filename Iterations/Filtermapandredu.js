fea4084dbc0e7913265dc4a20409b20c5fdc086e
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-11 23:14:55 +0530

    return value

--- Iterations/Filtermapandredu.js
+++ Iterations/Filtermapandredu.js
@@ -1,9 +1,25 @@
 const coding = ["js","ruby","java","pythone","cpp"];
 
-coding.forEach( (item) => {
-    console.log(item);
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+// } );
+
+// console.log(values); 
+// js
+// ruby
+// java
+// pythone
+// cpp
+// undefined
+
+
+
+
+const values = coding.forEach( (item) => {
+    // console.log(item);
+    return item;
 } );
 
-
+console.log(values);
 
 
