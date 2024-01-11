a6d2ff2fa3140c91a3f9238d9fa174116dc2041d
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-11 23:33:35 +0530

    num arrays

--- Iterations/Filtermapandredu.js
+++ Iterations/Filtermapandredu.js
@@ -25,8 +25,8 @@ const coding = ["js","ruby","java","pythone","cpp"];
 
 const myNums = [1,2,3,4,5,6,7,8,9,0];
 
-myNums.filter( (num) => num > 4 )
-
+const newNums = myNums.filter( (num) => num > 4 )
+console.log(newNums);
 
 
 
