18010a0cb2625c4840ae74c6edee603be5469344
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-11 23:36:22 +0530

    filter

--- Iterations/Filtermapandredu.js
+++ Iterations/Filtermapandredu.js
@@ -25,6 +25,10 @@ const coding = ["js","ruby","java","pythone","cpp"];
 
 const myNums = [1,2,3,4,5,6,7,8,9,0];
 
+// const newNums = myNums.filter( (num) => num > 4 )
+// console.log(newNums);
+
+
 const newNums = myNums.filter( (num) => num > 4 )
 console.log(newNums);
 
