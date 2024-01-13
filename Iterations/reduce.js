dcb8ecfb192a27e020fef96892b00957a20f50a2
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-13 18:20:40 +0530

    reduce with arrwo fun

--- Iterations/reduce.js
+++ Iterations/reduce.js
@@ -1,10 +1,14 @@
 const myNums = [1,2,3];
 
-const myTotal = myNums.reduce(function(acc ,currval ){
+// const myTotal = myNums.reduce(function(acc ,currval ){
     
-    console.log(`acc: ${acc} and currval: ${currval}`);
-    return acc + currval
-}, 3 );
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 3 );
+// console.log(myTotal);
+
+
+const myTotal = myNums.reduce( (acc , curr)=> acc + curr ,0);
 console.log(myTotal);
 
 
