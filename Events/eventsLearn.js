a8aea8485177583f3804ec02efde3b83cd7cce33
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-05 21:50:56 +0530

    events addEventListerner()

--- Events/eventsLearn.js
+++ Events/eventsLearn.js
@@ -1,6 +1,16 @@
 const nameh =document.getElementById('has');
 
 setInterval(function(){
-    nameh.innerHTML=`<h3>ha ji kaise hai app</h3>`
+    nameh.innerHTML=`<h3>Ha ji kaise hai app </h3>`
 },3000)
 
+
+// document.getElementById('owl').onclick = function(){alert("hey owl")}
+
+document.getElementById('owl').addEventListener('click',function(){
+    alert('owl clicked again');
+    console.log("owl click");
+},false)
+
+// attachEvent();
+// jQuery - 'on' evelnts  and react jsx
