0df7f4355477aea94d161421d8bee46d56584d6c
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-06 00:00:41 +0530

    remove()

--- Events/eventsLearn.js
+++ Events/eventsLearn.js
@@ -43,4 +43,6 @@ setInterval(function(){
 
 document.querySelector('#images').addEventListener('click',function(e){
     console.log(e.target.parentNode)
+    let removeIt = e.target.parentNode
+    removeIt.remove()
 },false)
